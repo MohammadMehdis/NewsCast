e38c1efd07b31a41bd0a9f93085da9de151af411
MohammadMehdis <your-email@example.com>
2024-08-23 22:01:37 +0330

    End nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      Oswald: "Oswald",
+      Sofia: "Sofia Sans",
+    },
     extend: {
-      colors: {},
-      fontFamily: {},
+      colors: {
+        black: {
+          100: "#393939",
+        },
+        while: {
+          100: "#FFFFFF",
+        },
+        red: {
+          100: "#F65050",
+        },
+      },
+      boxShadow: {
+        ShadowBlack: "0px 1px 10px rgba(246, 80, 80, 0.15)",
+      },
     },
   },
   plugins: [],
